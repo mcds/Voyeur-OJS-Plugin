2da01ce3d38cd97d75a9c6a7755f169a819f42f8
Corey Slavnik <corez9@gmail.com>
2010-12-06 15:14:16 -0500

    add dynamic tool option & banish .DS_Store

---
+++ js/voyeurHeadAdmin.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	// Run the tool initially.
+	voyeurUpdateOptions($('#voyeurSettingsTable'), $('#voyeurTool').val());
+	$('#voyeurTool').change(function() {
+		voyeurUpdateOptions($('#voyeurSettingsTable'), $('#voyeurTool').val());
+	});
+});
+
+function voyeurUpdateOptions(voyeurSettingsTable, currentTool) {
+	var fadeTime = 250;
+	if (currentTool == 'Bubbles' || currentTool == 'Reader' || currentTool == 'WordCountFountain') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeOut(fadeTime);
+	}
+	if (currentTool == 'Cirrus') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeIn(fadeTime);
+	}
+	if (currentTool == 'CorpusTypeFrequenciesGrid') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeIn(fadeTime);
+	}
+	if (currentTool == 'Links') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeIn(fadeTime);
+	}
+	if (currentTool == 'CorpusSummary') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeIn(fadeTime);
+	}
+	if (currentTool == 'Cirrus') {
+		voyeurSettingsTable.find('#removeFuncWords').closest('tr').fadeIn(fadeTime);
+	}
+}
